ce1847cf28005c17c3bbd53157d699042dffeead
nissemand243 <47781194+nissemand243@users.noreply.github.com>
2020-05-01 14:32:12 +0200

    commenting and workign code

--- practice3/main.js
+++ practice3/main.js
@@ -1,5 +1,37 @@
+var data = "ssssiiiiiimoonhfdsak"
 
-const names = ['John', 'Paul', 'George', 'Ringo', 'John'];
+var results = [""];
+var substring ="";
+//For loop, der kører fra starten af min string til slutningen og danner et array
+for (var i = 0; i < data.length; i++){
+    
+//hvis substring allerede indeholder dataelementet vil den fjerne det 
+    if(substring.includes(data[i])){
+        substring = substring
 
-let unique = [...new Set(names)];
-console.log(unique); // 'John', 'Paul', 'George', 'Ringo'
+        //slice returnerer elementerne defineret, det vil sige indexOf +1 vil betyde at det element den møder, der er en dublikant, vil blive skåret fra
+            .slice(
+                //indexOF returnerer positionen af elementet i array'et, hvor den møder et element, den allerede har tilføjet
+                substring.indexOf(data[i]) + 1,
+                substring.length
+            );
+
+    }
+//substring er lig det array der dannes af for loopet
+        substring += data[i];
+
+        //hvis substring er længere end results
+        if(substring.length > results[0].length){
+            //skal results danne et array over substring
+            results = [substring];
+
+            //ellers hvis substring er lige så lang som results
+        }else if(substring.length === results[0].length){
+            
+            //skal results pushe substring ind i arrayet
+            results.push(substring);
+        }
+     
+}
+
+console.log(results);
